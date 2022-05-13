2cd5ab22eedd23d8650c60d866906a501202ea9c
Lin Wenxiang <3065746398@qq.com>
2022-05-13 23:42:44 +0800

    5\13

--- oop.py
+++
@@ -1,9 +0,0 @@
-from numpy import real
-
-
-class Complex:
-    def __init__(self,realpart,imagpart):
-        self.r=realpart
-        self.i=imagpart
-x=Complex(2,3)
-print('{}+{}i'.format(x.r,x.i))
